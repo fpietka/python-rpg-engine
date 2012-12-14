99b26dcab30a94bd72411eb1cf6e48b8227c0900
François Pietka <francois@pietka.fr>
2012-12-14 23:56:33 +0100

    change builder itself

--- map/builder.py
+++ map/builder.py
@@ -43,40 +43,57 @@ tileset_properties = (
     )
 
 class Builder():
-    def load(self):
-        pygame.init()
+    def __init__(self):
+        self.level_map = open("map/level1.map", "rb").read().split("\n")
+        (self.height, self.width) = (len(self.level_map) * tileset_config['height'], len(self.level_map[0]) *
+                tileset_config['width'])
 
-        RESOLUTION = 800, 600
-        self.screen = pygame.display.set_mode(RESOLUTION)
+    def load(self, screen, (x, y)):
+        self.screen_size = screen.get_size()
+        self.build_tileset()
+        # display tiles
+        self.fond = pygame.Surface(self.screen_size)
+        return self.update((x, y))
 
-        # build tileset
-        self.fond = pygame.image.load(tileset_config['name']).convert()
+    def build_tileset(self):
+        "Build tile set"
+
+        fond = pygame.image.load(tileset_config['name']).convert()
         width = tileset_config['width']
         height = tileset_config['height']
-        tileset = list()
+        self.tileset = list()
         for index, map in enumerate(tileset_config['map']):
             top = map[0]
             left = map[1]
             rect = pygame.Rect(left, top, width, height)
-            subSurface = self.fond.subsurface(rect)
-            tileset.append(subSurface)
+            subSurface = fond.subsurface(rect)
+            self.tileset.append(subSurface)
+        return self
 
-        # display tiles
-        self.fond = pygame.Surface((8000, 6000))
-        # read map
-        # TODO read line by line?
-        # XXX start with current script root?
-        level_map = open("map/level1.map", "rb").read().split("\n")
-        for index_y, line in enumerate(level_map, 0):
-            for index_x, square in enumerate(line, 0):
+    def update(self, (x, y)):
+        "Build visible map"
+        (width, height) = self.screen_size
+
+        import math
+        x_pos = x % tileset_config['width']
+        y_pos = y % tileset_config['height']
+
+        # @TODO lamba or function for that
+        nb_width = int(math.ceil(float(x % tileset_config['width'] + width) / float(tileset_config['width'])))
+        nb_height = int(math.ceil(float(y % tileset_config['height'] + height) / float(tileset_config['height'])))
+
+        x_start = x / tileset_config['width']
+        y_start = y / tileset_config['height']
+
+        for index_y, line in enumerate(range(y_start, y_start + nb_height), 0):
+            for index_x, column in enumerate(range(x_start, x_start + nb_width), 0):
+                #print (line, column)
+                square = self.level_map[line][column]
                 if square == '#':
-                    tile = tileset[3]
+                    tile = self.tileset[3]
                 elif square == '.':
-                    tile = tileset[5]
-                # XXX handle there properties for neighbour tiles XXX
-                # XXX gecode?
-                self.fond.blit(tile, (index_x * 40, index_y * 40))
-
+                    tile = self.tileset[5]
+                self.fond.blit(tile, ((index_x * 40) - x_pos, (index_y * 40) - y_pos))
         return self.fond
 
     def make_problem(self, block_size=40):
