21af388d649ad38e54c359e4a1dcd1841273c781
Ghislain Rodrigues <contact@ghislain.rodrigues.de>
2013-01-23 00:07:28 +0100

    sprite module added to have static and dynamic sprites

---
+++ sprite.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+
+class StaticSprite(pygame.sprite.Sprite):
+    IS_STATIC = True
+
+class DynamicSprite(pygame.sprite.Sprite):
+    IS_STATIC = False
